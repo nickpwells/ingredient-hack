590f85607e411d1a973cf239ea45883ad11742bb
nickpwells <wells.nick53@gmail.com>
2015-08-09 19:54:41 -0700

    ajax call to api

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
+
 	var ingredientsArr = [];
 
+	//removes the placeholder in the submit form
 	$("#ingredients").on('click', function(e){
 		e.preventDefault();
 		this.placeholder='';
 	});
 
+	//adds ingredient to list
 	$("button[type='submit']").click(function(e){
 		e.preventDefault();
 		var ingredient = $("#ingredients").val().toLowerCase();
@@ -17,17 +20,36 @@ $(document).ready(function() {
 		$("#ingredients").val('');
 	});
 
+	//removes ingredient from list
 	$("#active-ingredients").on('click', '#active-ingredient', function(e){
 		e.preventDefault();
-		
+	
 		//removes item from ingredients array
 		var index = $(this).index();
 		ingredientsArr.splice(index, 1);
-		console.log(ingredientsArr);
+
 		$(this).remove();
 	});
 
+	//clears all ingredients from list
+	$("#clear").click(function(e){
+		e.preventDefault();
+		$("#active-ingredients").empty();
+		ingredientsArr.length = 0;
+	});
 
+
+	$.ajax({
+	    url: 'https://webknox-recipes.p.mashape.com/recipes/findByIngredients', // The URL to the API. You can get this in the API page of the API you intend to consume
+	    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+	    data: {"ingredients": "chicken", "number": "5"}, // Additional parameters here
+	    dataType: 'json',
+	    success: function(data) { console.log(data); },
+	    error: function(err) { alert(err); },
+	    beforeSend: function(xhr) {
+	    	xhr.setRequestHeader("X-Mashape-Authorization", "95cPWKn2s2mshrWZr7MapR0N7IKnp118VyEjsnHs22zu7SUara"); // Enter here your Mashape key
+    	}
+	});
 });
 
 function createIngredientButton (ingredient) {
